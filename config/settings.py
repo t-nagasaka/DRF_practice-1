d6c657d9c0daa38e68472879364326d53f213b39
t-nagasaka <66255777+t-nagasaka@users.noreply.github.com>
2021-10-03 04:04:12 +0900

    done create_template

--- config/settings.py
+++ config/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATE_DIR = BASE_DIR.joinpath('template')
+TEMPLATE_DIR = BASE_DIR.joinpath('templates')
 STATIC_DIR = BASE_DIR.joinpath('static')
 
 # Quick-start development settings - unsuitable for production
